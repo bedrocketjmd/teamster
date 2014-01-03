6805c657edd706786cd5f83b85d4859dbe3fd552
Shovan Joshi <shovanj@gmail.com>
2014-01-03 10:31:26 -0700

    rename package variable

--- lib/package.rb
+++ lib/package.rb
@@ -1,21 +1,21 @@
 class Package
-  attr_reader :package, :sprockets
+  attr_reader :package_config, :sprockets
 
-  def initialize(package)
-    @package = package
+  def initialize(package_config)
+    @package_config = package_config
     @sprockets = Sprockets::Environment.new('./') { |env| }
-    @package.asset_paths.each { |path| @sprockets.append_path( File.join( path ) ) }
+    @package_config.asset_paths.each { |path| @sprockets.append_path( File.join( path ) ) }
 
-    if @package.compress
+    if @package_config.compress
       @sprockets.js_compressor = Uglifier.new(:mangle => false)
       @sprockets.css_compressor = YUI::CssCompressor.new
     end
 
-    FileUtils.remove_dir(package.location, force: true)
-    FileUtils.mkdir_p(package.location + '/assets')
-    FileUtils.cp_r( Dir.glob('./public/*'), package.location )
-    FileUtils.cp_r( './app', package.location )
-    FileUtils.cp_r( Dir.glob('./assets/images/*'), "#{package.location}/assets" )
+    FileUtils.remove_dir(package_config.location, force: true)
+    FileUtils.mkdir_p(package_config.location + '/assets')
+    FileUtils.cp_r( Dir.glob('./public/*'), package_config.location )
+    FileUtils.cp_r( './app', package_config.location )
+    FileUtils.cp_r( Dir.glob('./assets/images/*'), "#{package_config.location}/assets" )
   end
 
   def pack
@@ -26,42 +26,42 @@ class Package
   end
 
   def build_dynamic_files
-    @package.dynamic_files.each do |file|
-      File.open("#{package.location}/#{file.path}", 'w')  { |f| f.write(file.content) }
+    @package_config.dynamic_files.each do |file|
+      File.open("#{package_config.location}/#{file.path}", 'w')  { |f| f.write(file.content) }
     end
   end
 
   def build_index_html
     data = ""
-    f = File.open("#{package.location}/index.html", "r")
+    f = File.open("#{package_config.location}/index.html", "r")
     f.each_line do |line|
       if line =~ /src=\"assets\/application.js/
         sprocket = sprockets['application.js']
-        digest_paths = (package.concatenate) ? [sprocket.digest_path] : sprocket.dependencies.map(&:digest_path)
+        digest_paths = (package_config.concatenate) ? [sprocket.digest_path] : sprocket.dependencies.map(&:digest_path)
         data += digest_paths.
-          collect { |js_file| "<script src=\"//#{package.host}/assets/#{js_file}\"></script>" }.
+          collect { |js_file| "<script src=\"//#{package_config.host}/assets/#{js_file}\"></script>" }.
           join("\n")
       else
         data += line
       end
     end
-    File.open("#{package.location}/index.html", 'w')  { |f| f.write data }
+    File.open("#{package_config.location}/index.html", 'w')  { |f| f.write data }
   end
 
   def compile_js
     printf "    => Compiling js assets ..."
 
-    if package.concatenate
+    if package_config.concatenate
       ['application.js'].each do |file|
         asset = sprockets[file]
-        outfile = Pathname.new("#{package.location}/assets").join(asset.digest_path)
+        outfile = Pathname.new("#{package_config.location}/assets").join(asset.digest_path)
         asset.write_to(outfile)
         asset.write_to("#{outfile}.gz")
       end
     else
       sprockets["application.js"].dependencies.each do |processed_asset|
-        outfile = Pathname.new("#{package.location}/assets").join(processed_asset.digest_path)
-        if package.compress
+        outfile = Pathname.new("#{package_config.location}/assets").join(processed_asset.digest_path)
+        if package_config.compress
           puts "Compressing js asset #{processed_asset.digest_path}."
           processed_asset.modified_write_to(outfile, Uglifier.compile(processed_asset.source, :mangle => false))
           processed_asset.modified_write_to("#{outfile}.gz", Uglifier.compile(processed_asset.source, :mangle => false))
@@ -78,7 +78,7 @@ class Package
     printf "    => Compiling css assets ..."
 
     ['application'].each do |file|
-      outfile   = Pathname.new("#{package.location}/assets").join("#{file}.css")
+      outfile   = Pathname.new("#{package_config.location}/assets").join("#{file}.css")
       asset     = sprockets["#{file}.scss"]
       asset.write_to(outfile)
       asset.write_to("#{outfile}.gz")
@@ -88,16 +88,16 @@ class Package
   end
 
   def deploy
-    s3_config = package.target_service_configuration
+    s3_config = package_config.target_service_configuration
     s3 = AWS::S3.new( access_key_id: s3_config.access_key_id,
                                               secret_access_key: s3_config.secret_access_key)
-    bucket = s3.buckets[package.target_service_uri]
+    bucket = s3.buckets[package_config.target_service_uri]
 
     print "Uploading site "
 
-    Dir.glob("#{package.location}/**/*").each do |file|
+    Dir.glob("#{package_config.location}/**/*").each do |file|
       if File.file?(file)
-        remote_file = file.gsub( "#{package.location}/", "" )
+        remote_file = file.gsub( "#{package_config.location}/", "" )
         print '.'
         s3_object = bucket.objects[remote_file]
         s3_object.write(file: file, content_type: MIME::Types.type_for(file).first)
